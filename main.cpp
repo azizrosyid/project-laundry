431f9a45e52902831587f7a23717cbfcd7996593
azizrosyid <azizrosyid783@gmail.com>
2020-12-09 14:37:51 +0700

    menambahkan menu pilihan fitur

--- main.cpp
+++ main.cpp
@@ -2,6 +2,12 @@
 using namespace std;
 
 int main() {
-    
+    cout << "Selamat Datang di Laundry" <<endl;
+    cout << "Silahkan Pilih Menu : " <<endl;
+    cout << "1. Tambah Pesanan"<<endl;
+    cout << "2. Lihat daftar Pesanan"<<endl;
+    cout << "3. Edit Pesanan"<<endl;
+    cout << "4. Hapus Pesanan"<<endl;
+
     return 0;
 }
