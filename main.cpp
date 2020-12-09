9b9c647d3101e44b6e9f6be2720f453704bae748
azizrosyid <azizrosyid783@gmail.com>
2020-12-09 15:13:32 +0700

    menambahkan input user untuk menambahkan data-data

--- main.cpp
+++ main.cpp
@@ -2,12 +2,41 @@
 using namespace std;
 
 int main() {
-    cout << "Selamat Datang di Laundry" <<endl;
-    cout << "Silahkan Pilih Menu : " <<endl;
-    cout << "1. Tambah Pesanan"<<endl;
-    cout << "2. Lihat daftar Pesanan"<<endl;
-    cout << "3. Edit Pesanan"<<endl;
-    cout << "4. Hapus Pesanan"<<endl;
+    int choice;
+    string customerName;
+    string customerAddress;
+    string customerPhone;
+    int itemNumber;
+    string itemDescription;
+    int itemKg;
+
+    cout << "Selamat Datang di Laundry" << endl;
+    cout << "1. Tambah Pesanan" << endl;
+    cout << "2. Lihat daftar Pesanan" << endl;
+    cout << "3. Edit Pesanan" << endl;
+    cout << "4. Hapus Pesanan" << endl;
+    cout << "Silahkan Pilih Menu : ";
+    cin >> choice;
+
+    if (choice == 1) {
+        cout << "Silahkan Masukkan Data Pelanggan" << endl;
+        cin.ignore();
+        cout << "Nama       : ";
+        getline(cin, customerName);
+        cout << "Alamat     : ";
+        getline(cin, customerAddress);
+        cout << "Nomor HP   : ";
+        getline(cin, customerPhone);
+
+        cout << "Silahkan Masukkan Data Pakaian"<<endl;
+        cout << "Jumlah Pakaian     : ";
+        cin >> itemNumber;
+        cout << "Deskripsi Pakaian  : ";
+        cin.ignore();
+        getline(cin,itemDescription);
+        cout << "Berat Pakaian (Kg) : ";
+        cin >> itemKg;
+    }
 
     return 0;
 }
